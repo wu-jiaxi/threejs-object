c83bf5fc7acc71b7886f8fe0fe575adc4c682db7
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2024-01-29 15:11:50 -0600

    swapped assets and public folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <h1 className="head-text">Three JS</h1>
-    </div>
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   );
 }
 
