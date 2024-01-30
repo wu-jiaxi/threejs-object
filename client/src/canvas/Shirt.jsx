6ad7e1db4725b454e3c0913f9f1764de87bedf25
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2024-01-29 20:02:42 -0600

    shirt design logo and lighting

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -7,12 +7,45 @@ import { Decal, useGLTF, useTexture } from "@react-three/drei";
 import state from "../store";
 
 const Shirt = () => {
-  const snap = useSnapShot(state);
+  const snap = useSnapshot(state);
   const { nodes, materials } = useGLTF("/shirt_baked.glb");
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
-  return <group>Shirt</group>;
+  useFrame((state, delta) =>
+    easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
+  );
+  const stateString = JSON.stringify(snap);
+  return (
+    <group key={stateString}>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      >
+        {snap.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          ></Decal>
+        )}
+        {snap.isLogoTexture && (
+          <Decal
+            position={[0, 0, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.2}
+            map={logoTexture}
+            depthTest={false}
+            depthWrite={true}
+          ></Decal>
+        )}
+      </mesh>
+    </group>
+  );
 };
 
 export default Shirt;
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,18 +1,24 @@
 import { Canvas } from "@react-three/fiber";
 import { Environment, Center } from "@react-three/drei";
 import Shirt from "./Shirt";
-
+import CameraRig from "./CameraRig";
+import Backdrop from "./Backdrop";
 const CanvasModel = () => {
   return (
-    <Canvas>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 10 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
       <ambientLight />
       <Environment preset="city" />
-      {/*   <CameraRig>*/}
-      {/*  <Backdrop />  */}
-      <Center>
-        <Shirt />
-      </Center>
-      {/* </CameraRig> */}
+      <CameraRig>
+        <Backdrop />
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
     </Canvas>
   );
 };
