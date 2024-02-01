7fa2b2c523c893978a5abc658f2edeb9ccb4a8e7
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2024-02-01 14:28:55 -0600

    working on integrating dalle and threejs

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -6,6 +6,10 @@ dotenv.config();
 
 const router = express.Router();
 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
+});
+
 router.route("/").get((req, res) => {
   res.status(200).json({ message: "Hello from DallE Routes" });
 });
@@ -22,14 +26,11 @@ router.route("/").post(async (req, res) => {
     });
 
     const image = response.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Something went wrong" });
   }
-});
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
 });
 
 export default router;
