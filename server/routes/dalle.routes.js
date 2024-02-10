1742b967a6ab831a9334c4d5543ae1dc8eb22f7f
wu-jiaxi <dev.jiaxiwu@gmail.com>
2024-02-09 22:36:35 -0500

    data

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -18,14 +18,12 @@ router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const response = await openai.createImage({
+    const image = await openai.images.generate({
+      model: "dall-e-3",
       prompt,
-      n: 1,
-      size: "1024x1024",
-      response_format: "b64_json",
     });
 
-    const image = response.data.data[0].b64_json;
+    //const image = response.data.data[0].b64_json;
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
